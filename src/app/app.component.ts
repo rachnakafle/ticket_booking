21cbfbc78a434546a4d990c0c5d8202e89238aa5
rachnakafle <rachanakafle99@gmail.com>
2022-10-17 17:26:43 +0545

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  total_amount: any;
+  amount: any;
   title = 'events_form';
+
+  constructor() {}
+
+  eventsForm = new FormGroup({});
+
+  totalTicketAmount() {
+    this.total_amount = this.amount * 1500;
+  }
 }
