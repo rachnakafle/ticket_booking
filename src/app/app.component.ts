f8af2afe8c2d9883828e3e9a780941e6f48f3191
Rachna Kafle <rachanakafle99@gmail.com>
2022-10-18 08:19:12 -0400

    Total No. of Tickets Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,16 +6,27 @@ import { FormGroup } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   total_amount: any;
-  amount: any;
   title = 'events_form';
+  total_tickets: any;
 
+  ngOnInit(): void {
+    this.totalTicketAmount();
+  }
   constructor() {}
 
   eventsForm = new FormGroup({});
 
+  changeTicketNo(e:any){
+    console.log(e.target.value);
+    this.total_tickets = e.target.value;
+  } 
+
   totalTicketAmount() {
-    this.total_amount = this.amount * 1500;
+    this.total_amount = (this.total_tickets * 1500);
+    console.log(this.total_amount);
+    
   }
+ 
 }
